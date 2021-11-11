702254f4e7f73fe5bfdaef2e74af1b32c9056c82
Artem Makogon <artem.makogon.nsk@gmail.com>
2021-11-11 18:49:48 +0300

    Initial Commit

---
+++ uitest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by Артём Макогон on 11.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Артём Макогон on 11.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
