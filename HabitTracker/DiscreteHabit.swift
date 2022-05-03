d53a5183db8cede32c15ea672c5e3861bf9154f9
Artem Makogon <artem.makogon.nsk@gmail.com>
2022-05-03 17:46:37 +0300

    basic view

--- HabitTracker/DiscreteHabit.swift
+++ HabitTracker/DiscreteHabit.swift
@@ -17,6 +17,34 @@ class DiscreteHabit: Identifiable, Codable {
     public func addDate(date: Date) {
         completed_days.insert(date);
     }
+
+    public func calculateTotalSuccess() -> Int {
+        let cur_date = Date.now
+//        for i in 0...completed_days.count - 1 {
+//            // TODO
+//        }
+        return 10; // TODO
+    }
+
+    private func calculateAditionalPercent(current_persent: Int) -> Int {
+        if (current_persent <= 2) {
+            return 5
+        }
+        if (current_persent <= 7) {
+            return 3
+        }
+        if (current_persent <= 15) {
+            return 2
+        }
+        return 1
+    }
+
+    private func calculateFailPercent(skip_uninterrupted_days: Int) -> Int {
+        if (skip_uninterrupted_days >= 17) {
+            return 100
+        }
+        return Int(floor(pow(1.2, Double(skip_uninterrupted_days - 1))))
+    }
     
     var id: UUID
     var title: String;
--- HabitTracker/DiscreteHabitView.swift
+++ HabitTracker/DiscreteHabitView.swift
@@ -10,21 +10,55 @@ import SwiftUI
 
 struct DiscreteHabitView: View {
     let habit: DiscreteHabit
-    
+
     var body: some View {
-        HStack {
-            VStack {
-                Text(habit.title)
-                    .font(.headline.bold())
-                    .padding(.top)
-                    .padding(.leading)
-                HStack {
-                }
-            }
-            .padding(.bottom)
-            .background(.gray)
-            .cornerRadius(10)
+        Button(action: {
+            print("Large Button tapped!")
         }
-        .frame(maxWidth: .infinity)
+        ,
+        label: {
+            HStack {
+                VStack {
+                    HStack {
+                        Text(habit.title)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding(.top)
+                                .padding(.leading)
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Text("\(habit.calculateTotalSuccess())%")
+                                .font(.title)
+                        Spacer()
+                        Button(action: {
+                            print("Small button tapped!")
+                        }, label: {
+                            Image(systemName: "1.circle.fill")
+                        })
+                        Button(action: {
+                        }, label: {
+                            Image(systemName: "1.circle.fill")
+                        })
+                        Button(action: {
+                        }, label: {
+                            Image(systemName: "1.circle.fill")
+                        })
+                        Button(action: {
+                        }, label: {
+                            Image(systemName: "1.circle.fill")
+                        })
+                    }
+                            .frame(width: UIScreen.main.bounds.width * 0.8)
+                }
+                        .padding(.bottom)
+                        .background(.gray)
+                        .cornerRadius(10)
+            }
+                    .frame(width: UIScreen.main.bounds.width * 0.9,
+                            height: UIScreen.main.bounds.height * 0.15)
+        }
+        )
     }
 }
