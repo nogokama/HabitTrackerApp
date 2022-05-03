94780cc5ff69ab691dc39109087a72139b0fc7b5
Artem Makogon <artem.makogon.nsk@gmail.com>
2022-05-03 18:49:49 +0300

    add primary view

--- HabitTracker/DiscreteHabitView.swift
+++ HabitTracker/DiscreteHabitView.swift
@@ -15,50 +15,69 @@ struct DiscreteHabitView: View {
         Button(action: {
             print("Large Button tapped!")
         }
-        ,
-        label: {
-            HStack {
-                VStack {
+                ,
+                label: {
                     HStack {
-                        Text(habit.title)
-                                .font(.title)
-                                .fontWeight(.bold)
-                                .padding(.top)
-                                .padding(.leading)
-                        Spacer()
+                        VStack {
+                            HStack {
+                                Text(habit.title)
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                        .padding(.top)
+                                        .padding(.leading)
+                                        .foregroundColor(.black)
+                                Spacer()
+                            }
+                            Spacer()
+                            HStack {
+                                Text("\(habit.calculateTotalSuccess())%")
+                                        .font(.title)
+                                        .foregroundColor(.black)
+                                Spacer()
+                                TappingButtonsView(habit: habit)
+                            }
+                                    .frame(width: UIScreen.main.bounds.width * 0.8)
+                        }
+                                .padding(.bottom)
+                                .background(.yellow)
+                                .cornerRadius(10)
                     }
-                    Spacer()
-                    HStack {
-                        Text("\(habit.calculateTotalSuccess())%")
-                                .font(.title)
-                        Spacer()
-                        Button(action: {
-                            print("Small button tapped!")
-                        }, label: {
-                            Image(systemName: "1.circle.fill")
-                        })
-                        Button(action: {
-                        }, label: {
-                            Image(systemName: "1.circle.fill")
-                        })
-                        Button(action: {
-                        }, label: {
-                            Image(systemName: "1.circle.fill")
-                        })
-                        Button(action: {
-                        }, label: {
-                            Image(systemName: "1.circle.fill")
-                        })
-                    }
-                            .frame(width: UIScreen.main.bounds.width * 0.8)
+                            .frame(width: UIScreen.main.bounds.width * 0.9,
+                                    height: UIScreen.main.bounds.height * 0.15)
                 }
-                        .padding(.bottom)
-                        .background(.gray)
-                        .cornerRadius(10)
-            }
-                    .frame(width: UIScreen.main.bounds.width * 0.9,
-                            height: UIScreen.main.bounds.height * 0.15)
-        }
         )
+    }
+}
+
+struct TappingButtonsView: View {
+    let habit: DiscreteHabit
+    let totalButtonsCount = 4
+    var body: some View {
+        HStack {
+            ForEach(0..<totalButtonsCount) { element in
+                SingleTypingButtonView(habit: habit,
+                        position: totalButtonsCount - element)
+            }
+        }
+    }
+}
+
+struct SingleTypingButtonView: View {
+    let habit: DiscreteHabit
+    let position: Int
+    @State var tapped = false
+    var body: some View {
+        Button(action: {
+            print("Hello from Habit \(habit.title), button number: \(position)")
+            tapped.toggle()
+        }, label: {
+            HStack {
+                if tapped {
+                    Image(systemName: "1.circle.fill")
+                } else {
+                    Image(systemName: "1.circle")
+                }
+            }
+        })
     }
 }
