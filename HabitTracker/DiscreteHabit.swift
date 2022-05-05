fec6b617589d165d90df31467563e4e7d29751f0
Artem Makogon <artem.makogon.nsk@gmail.com>
2022-05-05 12:51:24 +0300

    implement HabitDate

--- HabitTracker/DiscreteHabit.swift
+++ HabitTracker/DiscreteHabit.swift
@@ -7,6 +7,71 @@
 
 import Foundation
 
+class HabitDate: Hashable, Codable, Comparable {
+
+    static func ==(lhs: HabitDate, rhs: HabitDate) -> Bool {
+        lhs.day == rhs.day && lhs.month == rhs.month && lhs.year == rhs.year
+    }
+
+    static func < (lhs: HabitDate, rhs: HabitDate) -> Bool {
+        if lhs.year != rhs.year {
+            return lhs.year < rhs.year
+        } else if lhs.month != rhs.month {
+            return lhs.month < rhs.month
+        } else {
+            return lhs.day < rhs.day
+        }
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(day)
+        hasher.combine(month)
+        hasher.combine(year)
+    }
+
+    init(day: Int, month: Int, year: Int) {
+        self.day = day
+        self.month = month
+        self.year = year
+    }
+
+    init(date: Date) {
+        self.day = Calendar.current.component(.day, from: date)
+        self.month = Calendar.current.component(.month, from: date)
+        self.year = Calendar.current.component(.year, from: date)
+    }
+
+    private func getSwiftDate() -> Date {
+        let components = DateComponents(year: year, month: month, day: day)
+        return Calendar(identifier: .gregorian).date(from: components)!
+    }
+
+    public func getDaysDifference(otherDate: HabitDate) -> Int {
+        Calendar.current.dateComponents([.day],
+                from: getSwiftDate(),
+                to: otherDate.getSwiftDate()).day! - 1
+    }
+
+    static func getDateFromToday(byAddingDays: Int) -> HabitDate {
+        HabitDate(date: Calendar.current.date(byAdding: .day,
+                value: byAddingDays,
+                to: Date.now)!)
+    }
+
+    static func today() -> HabitDate {
+        HabitDate(date: Date.now)
+    }
+
+    static func yesterday() -> HabitDate {
+        getDateFromToday(byAddingDays: -1)
+    }
+
+    var day: Int
+    var month: Int
+    var year: Int
+}
+
+
 class DiscreteHabit: ObservableObject, Identifiable, Codable {
     init(title: String) {
         self.id = UUID()
@@ -17,97 +82,78 @@ class DiscreteHabit: ObservableObject, Identifiable, Codable {
         self.success = calculateTotalSuccess()
     }
 
-    private func getStringByDate(date: Date) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        return dateFormatter.string(from: date)
+    public func addDate(date: HabitDate) {
+        completed_days.insert(date);
     }
-
-    private func getDateByString(string: String) -> Date {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        return dateFormatter.date(from: string)!
-        // TODO check exceptions
-    }
-
     public func addDate(date: Date) {
-        completed_days.insert(getStringByDate(date: date));
+        completed_days.insert(HabitDate(date: date));
     }
 
+    private func removeDate(date: HabitDate) {
+        completed_days.remove(date)
+    }
     private func removeDate(date: Date) {
-        completed_days.remove(getStringByDate(date: date))
+        completed_days.remove(HabitDate(date: date))
     }
 
-    public func checkDateInCompletedDays(date: Date) -> Bool {
-        completed_days.contains(getStringByDate(date: date))
-    }
 
-    private func getDateFromToday(byAddingDays: Int) -> Date {
-        Calendar.current.date(byAdding: .day, value: byAddingDays, to: Date.now)!
-        // TODO check exceptions
+    public func checkDateInCompletedDays(date: HabitDate) -> Bool {
+        completed_days.contains(date)
     }
 
     public func unmarkPosition(position: Int) {
-        removeDate(date: getDateFromToday(byAddingDays: -position))
+        removeDate(date: HabitDate.getDateFromToday(byAddingDays: -position))
         recalculateSuccess()
         //FIXME each habit now must know about habitTracker
     }
 
     public func markPosition(position: Int) {
-        addDate(date: getDateFromToday(byAddingDays: -position))
+        addDate(date: HabitDate.getDateFromToday(byAddingDays: -position))
         recalculateSuccess()
         //FIXME each habit now must know about habitTracker
     }
 
     public func isPositionTapped(position: Int) -> Bool {
-        checkDateInCompletedDays(date: getDateFromToday(byAddingDays: -position))
+        completed_days.contains(HabitDate.getDateFromToday(byAddingDays: -position))
     }
 
     public func calculateTotalSuccess() -> Int {
-        var dates: Array<Date> = []
+        var dates: Array<HabitDate> = []
         for element in completed_days {
-            dates.append(getDateByString(string: element))
+            dates.append(element)
         }
-        if !completed_days.contains(getStringByDate(date: Date.now)) &&
-                   !completed_days.contains(
-                           getStringByDate(date: getDateFromToday(byAddingDays: -1))
-                   ) {
-            dates.append(getDateFromToday(byAddingDays: -1))
+        if !completed_days.contains(HabitDate.today()) &&
+                   !completed_days.contains(HabitDate.yesterday()) {
+            dates.append(HabitDate.yesterday())
         }
 
         dates.sort()
         var answer = 0
         for i in 0..<dates.count - 1 {
-            answer += calculateAditionalPercent(current_persent: answer)
+            answer += calculateAdditionalPercent(currentPercent: answer)
             answer -= calculateFailPercent(
-                    skip_uninterrupted_days: daysBetween(start: dates[i], end: dates[i + 1])
+                    skip_uninterrupted_days: dates[i].getDaysDifference(otherDate: dates[i + 1])
             )
             answer = max(0, answer)
         }
 
-        if completed_days.contains(getStringByDate(date: dates[dates.count - 1])) {
-            answer += calculateAditionalPercent(current_persent: answer)
+        if completed_days.contains(dates[dates.count - 1]) {
+            answer += calculateAdditionalPercent(currentPercent: answer)
         }
         return answer
     }
 
-    private func daysBetween(start: Date, end: Date) -> Int {
-        return Calendar.current.dateComponents([.day], from: start, to: end).day! - 1
-    }
 
     private func recalculateSuccess() {
         success = calculateTotalSuccess()
         print(success)
     }
 
-    private func calculateAditionalPercent(current_persent: Int) -> Int {
-        if (current_persent <= 2) {
+    private func calculateAdditionalPercent(currentPercent: Int) -> Int {
+        if (currentPercent <= 2) {
             return 5
         }
-        if (current_persent <= 7) {
-            return 3
-        }
-        if (current_persent <= 15) {
+        if (currentPercent <= 10) {
             return 2
         }
         return 1
@@ -122,7 +168,7 @@ class DiscreteHabit: ObservableObject, Identifiable, Codable {
 
     var id: UUID
     @Published var title: String
-    var completed_days: Set<String> {
+    var completed_days: Set<HabitDate> {
         didSet {
             habitTracker!.dumpAllData()
             // TODO check if nil
@@ -130,7 +176,7 @@ class DiscreteHabit: ObservableObject, Identifiable, Codable {
     }
 
     @Published var success: Int
-    var habitTracker:HabitTracker?
+    var habitTracker: HabitTracker?
 
     private enum CodingKeys: CodingKey {
         case id, title, completed_days
@@ -140,7 +186,7 @@ class DiscreteHabit: ObservableObject, Identifiable, Codable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(UUID.self, forKey: .id)
         title = try container.decode(String.self, forKey: .title)
-        completed_days = try container.decode(Set<String>.self, forKey: .completed_days)
+        completed_days = try container.decode(Set<HabitDate>.self, forKey: .completed_days)
         success = 0
         success = calculateTotalSuccess()
     }
